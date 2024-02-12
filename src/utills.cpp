8888f21b59e7b0fd44fa9f209000a381482db666
badgeminer <badgedog123@gmail.com>
2024-02-12 10:36:37 -0700

    document the math

--- src/utills.cpp
+++ src/utills.cpp
@@ -1,6 +1,10 @@
 #include "utills.h"
 
 double ConvertPCTdegC(double percent) {
-    //return (percent - 21.0) / (100 - 21.0) * 100;
+    //(percent)                           = (deg - 21.0) / (100 - 21.0) * 100;
+    //(percent/100)                       = (deg - 21.0) / (100 - 21.0)
+    //(percent/100) * (100 - 21.0)        = (deg - 21.0)
+    //(percent/100) * (100 - 21.0) + 21.0 = deg
+
     return ((percent/100)*(100 - 21.0))+21.0;
 }
