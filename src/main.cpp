aa3ef8794bb3e3497b88e000ffc1ee7448eec15b
badgeminer <badgedog123@gmail.com>
2024-02-13 08:48:12 -0700

    indent fixes

--- src/main.cpp
+++ src/main.cpp
@@ -77,17 +77,16 @@ struct buttons
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
-  LedR1.set(0);
-  LedR2.set(0);
-  LedY1.set(0);
-  LedY2.set(0);
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
-  Inertial.startCalibration();
-  wait(2000,timeUnits::msec);
-  std::cout << "Inertial Calibration" << std::endl;
-  Controller1.rumble(".");
-  
+    LedR1.set(0);
+    LedR2.set(0);
+    LedY1.set(0);
+    LedY2.set(0);
+    // All activities that occur before the competition starts
+    // Example: clearing encoders, setting servo positions, ...
+    Inertial.startCalibration();
+    wait(2000,timeUnits::msec);
+    std::cout << "Inertial Calibration" << std::endl;
+    Controller1.rumble(".");
 }
 
 
@@ -96,34 +95,34 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 int displayTask() {
     while(1) {
-      	// display some useful info
-      	Brain.Screen.setCursor(2,1);
-      	Brain.Screen.print( "  MotorLb    speed: %4.0f   position: %6.2f", MotorLb.velocity( percent ), MotorLb.position( rev ) );
-      	Brain.Screen.newLine();
-      	Brain.Screen.print( "  MotorLf    speed: %4.0f   position: %6.2f", MotorLf.velocity( percent ), MotorLf.position( rev ));
-      	Brain.Screen.newLine();
-      	Brain.Screen.print( "  MotorRb    speed: %4.0f   position: %6.2f", MotorRb.velocity( percent ), MotorRb.position( rev ));
-      	Brain.Screen.newLine();
-      	Brain.Screen.print( "  MotorRf    speed: %4.0f   position: %6.2f", MotorRf.velocity( percent ), MotorRf.position( rev ));
-      	Brain.Screen.newLine();
-      	Brain.Screen.newLine();
+        // display some useful info
+        Brain.Screen.setCursor(2,1);
+        Brain.Screen.print( "  MotorLb    speed: %4.0f   position: %6.2f", MotorLb.velocity( percent ), MotorLb.position( rev ) );
+        Brain.Screen.newLine();
+        Brain.Screen.print( "  MotorLf    speed: %4.0f   position: %6.2f", MotorLf.velocity( percent ), MotorLf.position( rev ));
+        Brain.Screen.newLine();
+        Brain.Screen.print( "  MotorRb    speed: %4.0f   position: %6.2f", MotorRb.velocity( percent ), MotorRb.position( rev ));
+        Brain.Screen.newLine();
+        Brain.Screen.print( "  MotorRf    speed: %4.0f   position: %6.2f", MotorRf.velocity( percent ), MotorRf.position( rev ));
+        Brain.Screen.newLine();
+        Brain.Screen.newLine();
 
-      	// motor group velocity and position is returned for the first motor in the group
-      	Brain.Screen.print( "  leftDrive  speed: %4.0f   position: %6.2f", LeftDrive.velocity( percent ), LeftDrive.position( rev ));
-      	Brain.Screen.newLine();
-      	Brain.Screen.print( "  rightDrive speed: %4.0f   position: %6.2f", RightDrive.velocity( percent ), RightDrive.position( rev ));
-      	Brain.Screen.newLine();
-      	Brain.Screen.newLine();
+        // motor group velocity and position is returned for the first motor in the group
+        Brain.Screen.print( "  leftDrive  speed: %4.0f   position: %6.2f", LeftDrive.velocity( percent ), LeftDrive.position( rev ));
+        Brain.Screen.newLine();
+        Brain.Screen.print( "  rightDrive speed: %4.0f   position: %6.2f", RightDrive.velocity( percent ), RightDrive.position( rev ));
+        Brain.Screen.newLine();
+        Brain.Screen.newLine();
 
-      	// drivetrain velocity is the average of the motor velocities for left and right
-      	Brain.Screen.print( "  Drivetrain speed: %4.0f", Drivetrain.velocity( percent ) );
-      	Brain.Screen.newLine();
+        // drivetrain velocity is the average of the motor velocities for left and right
+        Brain.Screen.print( "  Drivetrain speed: %4.0f", Drivetrain.velocity( percent ) );
+        Brain.Screen.newLine();
 
-      	Controller1.Screen.setCursor(1,1);
-      	Controller1.Screen.print("Temp %.1f",ConvertPCTdegC(Drivetrain.temperature(percent)));
+        Controller1.Screen.setCursor(1,1);
+        Controller1.Screen.print("Temp %.1f",ConvertPCTdegC(Drivetrain.temperature(percent)));
 
-      	// no need to run this loop too quickly
-      	wait( 20, timeUnits::msec );
+        // no need to run this loop too quickly
+        wait( 20, timeUnits::msec );
     }
 
     return 0;
@@ -132,20 +131,20 @@ int displayTask() {
 int logTask() {
 	if (Brain.SDcard.isInserted()) {
 		uint8_t tempBuf[0];
-    	Brain.SDcard.savefile("match.bin", tempBuf, 0);
+      Brain.SDcard.savefile("match.bin", tempBuf, 0);
 		while (1)
 		{
-    	  	union thing {
-    	  	    uint8_t result[20];  // 4 per int  // 12 for 3
-    	  	    struct loggedData {
-    	  	      int Lf_temp;
-    	  	      int Lb_temp;
-    	  	      int Rf_temp;
+          union thing {
+              uint8_t result[20];  // 4 per int  // 12 for 3
+              struct loggedData {
+                int Lf_temp;
+                int Lb_temp;
+                int Rf_temp;
 				  int Rb_temp;
 				  int Puncher_temp;
-    	  	    } motors;
-    	  	} t;
-    	  	t.motors.Lf_temp      = MotorLf.temperature(temperatureUnits::celsius);
+              } motors;
+          } t;
+          t.motors.Lf_temp      = MotorLf.temperature(temperatureUnits::celsius);
 			t.motors.Lb_temp      = MotorLb.temperature(temperatureUnits::celsius);
 			t.motors.Rf_temp      = MotorRf.temperature(temperatureUnits::celsius);
 			t.motors.Rb_temp      = MotorRb.temperature(temperatureUnits::celsius);
@@ -170,27 +169,27 @@ int logTask() {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  	display(0b0011);
-  	// ..........................................................................
-  	// Insert autonomous user code here.
-  	// ..........................................................................
-  	LeftDrive.spin(forward,200,velocityUnits::rpm);
-  	RightDrive.spin(forward,200,velocityUnits::rpm);
-  	wait( 1300, timeUnits::msec );
+    display(0b0011);
+    // ..........................................................................
+    // Insert autonomous user code here.
+    // ..........................................................................
+    LeftDrive.spin(forward,200,velocityUnits::rpm);
+    RightDrive.spin(forward,200,velocityUnits::rpm);
+    wait( 1300, timeUnits::msec );
 
 	MotorPuncher.spin(forward,200,velocityUnits::rpm);
 	wait( 200, timeUnits::msec );
 
-  	LeftDrive.spin(reverse,100,velocityUnits::rpm);
-  	RightDrive.spin(reverse,100,velocityUnits::rpm);
-  	wait( 500, timeUnits::msec );
+    LeftDrive.spin(reverse,100,velocityUnits::rpm);
+    RightDrive.spin(reverse,100,velocityUnits::rpm);
+    wait( 500, timeUnits::msec );
 
-  	LeftDrive.spin(reverse,200,velocityUnits::rpm);
-  	RightDrive.spin(reverse,200,velocityUnits::rpm);
-  	wait( 1000, timeUnits::msec );
+    LeftDrive.spin(reverse,200,velocityUnits::rpm);
+    RightDrive.spin(reverse,200,velocityUnits::rpm);
+    wait( 1000, timeUnits::msec );
 
 	MotorPuncher.stop(brakeType::coast);
-  	Drivetrain.stop(brakeType::brake);
+    Drivetrain.stop(brakeType::brake);
 }
 
 /*---------------------------------------------------------------------------*/
@@ -200,22 +199,22 @@ void autonomous(void) {
 /*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
-  	// User control code here, inside the loop
+    // User control code here, inside the loop
 	MotorPuncher.stop(brakeType::coast);
-  	Drivetrain.stop(brakeType::brake);
-  	while (1) {
-    	// This is the main execution loop for the user control program.
-    	// Each time through the loop your program should update motor + servo
-    	// values based on feedback from the joysticks.
+    Drivetrain.stop(brakeType::brake);
+    while (1) {
+      // This is the main execution loop for the user control program.
+      // Each time through the loop your program should update motor + servo
+      // values based on feedback from the joysticks.
 
-    	// ........................................................................
-    	// Insert user code here. This is where you use the joystick values to
-    	// update your motors, etc.
-    	// ........................................................................
+      // ........................................................................
+      // Insert user code here. This is where you use the joystick values to
+      // update your motors, etc.
+      // ........................................................................
 		L = Controller1.Axis3.position();
-    	R = Controller1.Axis2.position();
-    	LeftDrive.spin(forward,L,percent);
-    	RightDrive.spin(forward,R,percent);
+      R = Controller1.Axis2.position();
+      LeftDrive.spin(forward,L,percent);
+      RightDrive.spin(forward,R,percent);
 
 		// Puncher
 		if (Controller1.ButtonR2.pressing()) {
@@ -244,9 +243,9 @@ void usercontrol(void) {
 			Buttons.up = false;
 		}
 
-    	wait(20, msec); // Sleep the task for a short amount of time to
-    	                // prevent wasted resources.
-  	}
+      wait(20, msec); // Sleep the task for a short amount of time to
+                      // prevent wasted resources.
+    }
 }
 
 // Main will set up the competition functions and callbacks.
@@ -257,11 +256,11 @@ int main() {
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
 
-  	// Run the pre-autonomous function.
-  	pre_auton();
+    // Run the pre-autonomous function.
+    pre_auton();
 
-  	// Prevent main from exiting with an infinite loop.
-  	while (true) {
-  	  	wait(100, msec);
-  	}
+    // Prevent main from exiting with an infinite loop.
+    while (true) {
+        wait(100, msec);
+    }
 }
